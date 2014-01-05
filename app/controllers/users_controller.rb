8aaa65ebf0e09799905a65cd49810554ea6d9eb3
akimono <frank.cioppettini@gmail.com>
2014-01-05 16:02:21 -0500

    adlkjf

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,9 @@ class UsersController < ApplicationController
   # GET /users
   # GET /users.json
   def index
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @users = User.all
 
     respond_to do |format|
@@ -9,16 +12,21 @@ class UsersController < ApplicationController
       format.json { render json: @users }
     end
   end
+  end
 
   # GET /users/1
   # GET /users/1.json
   def show
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @user = User.find(params[:id])
 
     respond_to do |format|
       format.html # show.html.erb
       format.json { render json: @user }
     end
+  end
   end
 
   # GET /users/new
@@ -29,17 +37,24 @@ class UsersController < ApplicationController
     respond_to do |format|
       format.html # new.html.erb
       format.json { render json: @user }
-    end
+  end
   end
 
   # GET /users/1/edit
   def edit
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @user = User.find(params[:id])
+  end
   end
 
   # POST /users
   # POST /users.json
   def create
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @user = User.new(params[:user])
 
     respond_to do |format|
@@ -52,10 +67,14 @@ class UsersController < ApplicationController
       end
     end
   end
+  end
 
   # PUT /users/1
   # PUT /users/1.json
   def update
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @user = User.find(params[:id])
 
     respond_to do |format|
@@ -67,11 +86,15 @@ class UsersController < ApplicationController
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
+    end
   end
 
   # DELETE /users/1
   # DELETE /users/1.json
   def destroy
+    if session[:user] == nil
+      redirect_to action: 'new'
+    else
     @user = User.find(params[:id])
     @user.destroy
 
@@ -79,5 +102,6 @@ class UsersController < ApplicationController
       format.html { redirect_to users_url }
       format.json { head :no_content }
     end
+  end
   end
 end
