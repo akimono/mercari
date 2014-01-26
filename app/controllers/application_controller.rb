6b64e6e33df1fae0637ce619c1d220b987b3b656
akimono <frank.cioppettini@gmail.com>
2014-01-26 17:33:49 -0500

    revert attempt 2

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,10 +1,3 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
-  def default_url_options
-    if Rails.env.production?
-      {:host => "http://www.mercari.herokuapp.com"}
-    else  
-      {}
-    end
-  end
 end
