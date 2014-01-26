4407a74ac8026455240cb5dd473dea1f6bad1738
akimono <frank.cioppettini@gmail.com>
2014-01-26 17:29:07 -0500

    mail url 3

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  def default_url_options
+    if Rails.env.production?
+      {:host => "http://www.mercari.herokuapp.com"}
+    else  
+      {}
+    end
+  end
 end
