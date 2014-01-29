ed07436cff4d3e06ee5cd85d7b413e8a82d99f75
akimono <frank.cioppettini@gmail.com>
2014-01-28 21:34:23 -0500

    redirect fix

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -57,8 +57,8 @@ end
     respond_to do |format|
       if @query.save
         QueryMailer.new_query_email(@query).deliver
-        format.html { redirect_to @query, notice: 'Query was successfully created.' }
-        format.json { render json: @query, notice: 'Query was successfully created', status: :created, location: @query }
+        format.html { redirect_to @query, action: "new", notice: 'Query was successfully created.' }
+        format.json { render json: @query, status: :created, location: @query }
       else
         format.html { render action: "new" }
         format.json { render json: @query.errors, status: :unprocessable_entity }
