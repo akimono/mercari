cf9de746ae4e185e9aa0a007804df5b5876d62d1
akimono <frank.cioppettini@gmail.com>
2014-01-28 21:57:57 -0500

    reformat flash

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -57,7 +57,8 @@ end
     respond_to do |format|
       if @query.save
         QueryMailer.new_query_email(@query).deliver
-        format.html { flash[:notice] = 'Query was successfully created.', redirect_to action: "new" }
+        flash[:notice] = 'Query was successfully created.'
+        format.html { redirect_to action: "new" }
         format.json { render json: @query, status: :created, location: @query }
       else
         format.html { render action: "new" }
