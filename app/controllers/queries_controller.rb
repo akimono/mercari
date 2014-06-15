32d4a329071f19fc729b0f47cba191bbb8a7ad58
akimono <frank.cioppettini@gmail.com>
2014-06-15 13:03:09 -0400

    query admin email

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -58,6 +58,7 @@ end
     respond_to do |format|
       if @query.save
         QueryMailer.new_query_email(@query).deliver
+        QueryMailer.admin_query_email(@query).deliver
         flash[:notice] = 'Your query was successfully created. You should receive an email confirmation shortly'
         format.html { redirect_to action: "new" }
         format.json { render json: @query, status: :created, location: @query }
