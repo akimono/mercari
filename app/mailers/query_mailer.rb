b61197022d9a6eca2e7f9d7c06540de922b91483
akimono <frank.cioppettini@gmail.com>
2014-06-15 12:23:37 -0400

    email

--- app/mailers/query_mailer.rb
+++ app/mailers/query_mailer.rb
@@ -3,9 +3,14 @@ class QueryMailer < ActionMailer::Base
   def new_query_email(query)
   	@query = query
   	mail(to: @query.email, subject: 'Your Query has Successfully Been Created')
+  	mail(to: admin@mercariamercia.com, subject: "new query")
   end
   def update_query_email(query)
   	@query = query
+  	prebroken = @query.rep
+  	broken = prebroken.split("\s")
+  	repemail = broken.select{|each| "@" == each}
   	mail(to: @query.email, subject: 'Your Query has Been Updated')
+  	mail(to: repemail, subject: 'Update to query assigned to you')
   end
 end
