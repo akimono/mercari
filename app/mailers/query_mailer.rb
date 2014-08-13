f3ead2d0943995aee7c603286a2fe59c0cd1463f
akimono <frank.cioppettini@gmail.com>
2014-08-13 03:52:20 -0400

    spanish update

--- app/mailers/query_mailer.rb
+++ app/mailers/query_mailer.rb
@@ -6,12 +6,16 @@ class QueryMailer < ActionMailer::Base
   	end
      def new_spanish_query_email(query)
     @query = query
-    mail(to: @query.email, subject: 'Su Pregunta éxito ha sido creado')
+    mail(to: @query.email, subject: 'Su consulta éxito ha sido creado')
     end
   def update_query_email(query)
   	@query = query
   	mail(to: @query.email, subject: 'Your Query has Been Updated')
   	end
+    def update_spanish_query_email(query)
+    @query = query
+    mail(to: @query.email, subject: 'Su consulta ha sido actualizado')
+    end
   	def admin_query_email(query)
   		@query = query
   		mail(to: "admin@mercariamerica.com", subject: 'A new query has been created')
