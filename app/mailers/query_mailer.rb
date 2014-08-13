0ef241444aede09efc0d9250f05aae99d8286ac8
akimono <frank.cioppettini@gmail.com>
2014-08-13 02:18:44 -0400

    spanish email 2

--- app/mailers/query_mailer.rb
+++ app/mailers/query_mailer.rb
@@ -4,6 +4,10 @@ class QueryMailer < ActionMailer::Base
   	@query = query
   	mail(to: @query.email, subject: 'Your Query has Successfully Been Created')
   	end
+     def new_spanish_query_email(query)
+    @query = query
+    mail(to: @query.email, subject: 'Su Pregunta éxito ha sido creado')
+    end
   def update_query_email(query)
   	@query = query
   	mail(to: @query.email, subject: 'Your Query has Been Updated')
