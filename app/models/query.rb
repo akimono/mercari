8975092a481bc3d2e9d6cd4757169f49d3ce7a34
akimono <frank.cioppettini@gmail.com>
2014-09-08 19:20:05 -0400

    telephone

--- app/models/query.rb
+++ app/models/query.rb
@@ -1,6 +1,6 @@
 class Query < ActiveRecord::Base
   attr_accessible :amount, :budget, :businessname, :company, :destination, :email, :file1, :file2, :file3, :file4, :fromcountry, :name, :orderdetails, :product, :productinfo, :rep, :shippingtype, :status, :email_confirmation
-validates :amount, :name, :destination, :product, :shippingtype, :fromcountry, presence: true
+validates :amount, :name, :destination, :product, :shippingtype, :fromcountry, :budget, :file3, presence: true
 validates :email, confirmation: true, presence: true, length: { minimum: 3}
 validates :email_confirmation, presence: true, :on => :create
 
